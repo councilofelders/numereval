d9003e6485976a374e9fafb0ca715ddfced54b8b
parmarsuraj99 <parmarsuraj99@gmail.com>
2020-12-23 19:32:23 +0530

    update package name

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="numereval",
     version="0.0.1",
     author="Suraj Parmar",
     author_email="parmarsuraj99@gmail.com",
     description="A small package for evaluating numer.ai model locally",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/parmarsuraj99/numereval",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
